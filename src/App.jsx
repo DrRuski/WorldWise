7499c2d07e7048c16f006bb4efdec96363b06bee
DrRuski <ilia_e@hotmail.com>
2024-01-01 20:04:15 +0100

    Eod

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 export default function App() {
   return (
@@ -12,6 +14,13 @@ export default function App() {
         <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
