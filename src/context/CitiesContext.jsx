b27570c9009460be605322b6d8647ac984c4b865
DrRuski <ilia_e@hotmail.com>
2024-01-04 12:24:39 +0100

    gaming time

---
+++ src/context/CitiesContext.jsx
@@ -0,0 +1,48 @@
+import { createContext, useContext } from "react";
+import { useEffect, useState } from "react";
+const BASE_URL = "http://localhost:8000";
+
+const CitiesContext = createContext();
+
+export function CitiesProvide({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        console.error(error);
+        throw new Error("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        setCities,
+        isLoading,
+        setIsLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) {
+    throw new Error("Context was used outside its Provider");
+  }
+  return context;
+}
